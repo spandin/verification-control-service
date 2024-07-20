2bf5b8a9fce4813772057b740912e50dfeba0899
Stanislau <willstesi@gmail.com>
2024-07-20 23:47:33 +0300

    feat: refactoring

--- frontend/src/feauters/auth/login/ui/login-form.tsx
+++ frontend/src/feauters/auth/login/ui/login-form.tsx
@@ -1,80 +1,117 @@
 import { Button } from "@nextui-org/button"
 import { Input } from "@nextui-org/input"
-import { EyeOff, Eye } from "lucide-react"
-import { useState } from "react"
-import { useFormik } from "formik"
+import { Formik } from "formik"
 import * as Yup from "yup"
 import { useAuth } from "@/shared/hooks/use-auth"
+import PasswordInput from "@/shared/ui/password-input"
+import { SITE_CONFIG } from "@/shared/config/site"
+import { PopoverTrigger, PopoverContent, Popover } from "@nextui-org/react"
+
+const validationSchema = Yup.object({
+  email: Yup.string()
+    .email("Введите корректный email адрес.")
+    .required("Email обязятелен для заполнения.")
+    .min(5, "Минимальная длинна email - 5 символов.")
+    .max(32, "Максимальная длинна пароля - 32 символа."),
+  password: Yup.string()
+    .required("Пароль обязятелен для заполнения.")
+    .min(6, "Минимальная длинна пароля - 6 символов.")
+    .max(32, "Максимальная длинна пароля - 32 символа."),
+})
 
 export const LoginForm = () => {
   const { login } = useAuth()
-  const [isVisible, setIsVisible] = useState(false)
-
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-    },
-
-    validationSchema: Yup.object({
-      email: Yup.string()
-        .email("Введите корректный email адрес.")
-        .required("Email обязятелен для заполнения.")
-        .min(5, "Минимальная длинна email - 5 символов.")
-        .max(32, "Максимальная длинна пароля - 32 символа."),
-      password: Yup.string()
-        .required("Пароль обязятелен для заполнения.")
-        .min(6, "Минимальная длинна пароля - 6 символов.")
-        .max(32, "Максимальная длинна пароля - 32 символа."),
-    }),
-
-    onSubmit: async (values) => {
-      login(values.email, values.password)
-    },
-  })
 
   return (
-    <div>
-      <form onSubmit={formik.handleSubmit} noValidate>
-        <Input
-          className="max-w-xs"
-          variant="bordered"
-          type="email"
-          name="email"
-          label="Email"
-          placeholder="you@mail.com"
-          onChange={formik.handleChange}
-          value={formik.values.email}
-        />
-        <Input
-          className="max-w-xs"
-          variant="bordered"
-          type={isVisible ? "text" : "password"}
-          name="password"
-          label="Пороль"
-          placeholder="Пороль"
-          onChange={formik.handleChange}
-          value={formik.values.password}
-          endContent={
-            <button
-              className="focus:outline-none"
-              type="button"
-              onClick={() => setIsVisible(!isVisible)}
-              aria-label="toggle password visibility"
-            >
-              {isVisible ? (
-                <EyeOff className="pointer-events-none text-2xl text-default-400" />
-              ) : (
-                <Eye className="pointer-events-none text-2xl text-default-400" />
-              )}
-            </button>
-          }
-        />
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+      }}
+      validationSchema={validationSchema}
+      onSubmit={(values, { resetForm }) => {
+        login(values.email, values.password)
 
-        <Button type="submit" color="primary">
-          Войти
-        </Button>
-      </form>
-    </div>
+        resetForm()
+      }}
+    >
+      {({ errors, values, handleChange, handleBlur, handleSubmit }) => (
+        <form className="flex w-full max-w-sm flex-col gap-2" onSubmit={handleSubmit} noValidate>
+          <div className="mb-4 flex flex-col items-start justify-start px-1">
+            <div className="flex flex-row items-center justify-start gap-3">
+              <div className="h-4 w-4 rotate-45 bg-warning" />
+              <h1 className="text-2xl font-semibold text-slate-800">{SITE_CONFIG.description}</h1>
+            </div>
+
+            <h3 className="text-md font-semibold text-slate-500">
+              система контроля поверки ГУ "ЦОДБО"
+            </h3>
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Input
+              variant="bordered"
+              size="lg"
+              type="email"
+              name="email"
+              label="Email"
+              placeholder="you@mail.com"
+              onChange={handleChange}
+              value={values.email}
+            />
+
+            <PasswordInput
+              variant="bordered"
+              size="lg"
+              name="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+            />
+          </div>
+
+          <span className="text-md px-1 font-medium text-danger">
+            {errors.email || errors.password}
+          </span>
+
+          <Button className="font-medium" variant="shadow" color="primary" size="lg" type="submit">
+            Войти
+          </Button>
+
+          <Popover
+            showArrow
+            backdrop="blur"
+            placement="bottom"
+            classNames={{
+              base: ["before:bg-default-200"],
+              content: [
+                "py-3 px-4 border border-default-200 max-w-sm",
+                "bg-gradient-to-br from-white to-default-300",
+                "dark:from-default-100 dark:to-default-50",
+              ],
+            }}
+          >
+            <PopoverTrigger>
+              <Button className="font-semibold" variant="light" size="lg">
+                Помощь
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent>
+              {(titleProps) => (
+                <div className="px-1 py-2">
+                  <h3 className="text-base font-semibold" {...titleProps}>
+                    Как войти?
+                  </h3>
+                  <div className="text-md">
+                    Обратитесь к управляющим, возможно ваши данные для входа уже высланы на ваш
+                    Email
+                  </div>
+                </div>
+              )}
+            </PopoverContent>
+          </Popover>
+        </form>
+      )}
+    </Formik>
   )
 }
--- frontend/src/feauters/auth/register/ui/register-form.tsx
+++ frontend/src/feauters/auth/register/ui/register-form.tsx
@@ -1,126 +1,118 @@
-import { Button } from "@nextui-org/button"
+import { Button } from "@nextui-org/react"
 import { Select, SelectItem } from "@nextui-org/react"
 import { Input } from "@nextui-org/input"
-import { EyeOff, Eye } from "lucide-react"
-import { useState } from "react"
-import { useFormik } from "formik"
+import { Formik } from "formik"
 import * as Yup from "yup"
 import { useAuth } from "@/shared/hooks/use-auth"
 import { Role } from "@/shared/types"
+import PasswordInput from "@/shared/ui/password-input"
+import PhoneInput from "./compose/phone-input"
+
+const validationSchema = Yup.object({
+  name: Yup.string()
+    .required("Имя обязательно для заполнения.")
+    .min(2, "Имя должно быть больше двух букв."),
+  phone: Yup.string().nullable().min(5, "Имя должно быть больше двух букв."),
+  email: Yup.string()
+    .email("Введите корректный email адрес.")
+    .required("Email обязятелен для заполнения.")
+    .min(5, "Минимальная длинна email - 5 символов.")
+    .max(32, "Максимальная длинна пароля - 32 символа."),
+  password: Yup.string()
+    .required("Пароль обязятелен для заполнения.")
+    .min(6, "Минимальная длинна пароля - 6 символов.")
+    .max(32, "Максимальная длинна пароля - 32 символа."),
+  role: Yup.string().required("Выберите роль"),
+})
 
 export const RegisterForm = () => {
   const { register } = useAuth()
-  const [isVisible, setIsVisible] = useState(false)
-
-  const formik = useFormik({
-    initialValues: {
-      phone: null,
-      name: "",
-      email: "",
-      password: "",
-      role: Role.USER,
-    },
-
-    validationSchema: Yup.object({
-      name: Yup.string()
-        .required("Имя обязательно для заполнения.")
-        .min(2, "Имя должно быть больше двух букв."),
-      email: Yup.string()
-        .email("Введите корректный email адрес.")
-        .required("Email обязятелен для заполнения.")
-        .min(5, "Минимальная длинна email - 5 символов.")
-        .max(32, "Максимальная длинна пароля - 32 символа."),
-      password: Yup.string()
-        .required("Пароль обязятелен для заполнения.")
-        .min(6, "Минимальная длинна пароля - 6 символов.")
-        .max(32, "Максимальная длинна пароля - 32 символа."),
-      role: Yup.string().required("Выберите роль"),
-    }),
-
-    onSubmit: async (values) => {
-      register(
-        values.phone,
-        values.name,
-        values.email,
-        values.password,
-        values.role,
-      )
-
-      console.log("values", values)
-    },
-  })
 
   return (
-    <div>
-      <form
-        className="flex max-w-xs flex-col gap-1"
-        onSubmit={formik.handleSubmit}
-        noValidate
-      >
-        <Input
-          className="max-w-xs"
-          variant="bordered"
-          type="text"
-          name="name"
-          label="Имя"
-          placeholder="Введите имя"
-          onChange={formik.handleChange}
-          value={formik.values.name}
-        />
+    <Formik
+      initialValues={{
+        phone: null,
+        name: "",
+        email: "",
+        password: "",
+        role: Role.USER,
+      }}
+      validationSchema={validationSchema}
+      onSubmit={(values, { resetForm }) => {
+        register(values.phone, values.name, values.email, values.password, values.role)
 
-        <Input
-          className="max-w-xs"
-          variant="bordered"
-          type="email"
-          name="email"
-          label="Email"
-          placeholder="you@mail.com"
-          onChange={formik.handleChange}
-          value={formik.values.email}
-        />
+        resetForm()
+      }}
+    >
+      {({ errors, values, handleChange, handleBlur, handleSubmit }) => (
+        <form className="flex max-w-sm flex-col gap-2" onSubmit={handleSubmit} noValidate>
+          <div className="flex flex-col gap-2">
+            <Input
+              variant="bordered"
+              size="lg"
+              type="text"
+              name="name"
+              label="Имя"
+              placeholder="Сотрудник"
+              onChange={handleChange}
+              value={values.name}
+            />
 
-        <Input
-          className="max-w-xs"
-          variant="bordered"
-          type={isVisible ? "text" : "password"}
-          name="password"
-          label="Пороль"
-          placeholder="Пороль"
-          onChange={formik.handleChange}
-          value={formik.values.password}
-          endContent={
-            <button
-              className="focus:outline-none"
-              type="button"
-              onClick={() => setIsVisible(!isVisible)}
-              aria-label="toggle password visibility"
+            <PhoneInput values={values} handleChange={handleChange} handleBlur={handleBlur} />
+
+            <Input
+              variant="bordered"
+              size="lg"
+              type="email"
+              name="email"
+              label="Email"
+              placeholder="you@mail.com"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+            />
+
+            <PasswordInput
+              variant="bordered"
+              size="lg"
+              name="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+            />
+
+            <Select
+              variant="bordered"
+              size="lg"
+              name="role"
+              label="Должность"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.role}
             >
-              {isVisible ? (
-                <EyeOff className="pointer-events-none text-2xl text-default-400" />
-              ) : (
-                <Eye className="pointer-events-none text-2xl text-default-400" />
-              )}
-            </button>
-          }
-        />
+              {Object.values(Role).map((role) => (
+                <SelectItem key={role}>
+                  {role === Role.ADMIN
+                    ? "Администратор"
+                    : role === Role.ELECTRICAL_ENGINEER
+                      ? "Инженер-электрик"
+                      : role === Role.ENERGY_ENGINEER
+                        ? "Инженер-электрик"
+                        : "Пользователь"}
+                </SelectItem>
+              ))}
+            </Select>
+          </div>
 
-        <Select
-          className="max-w-xs"
-          variant="bordered"
-          name="role"
-          label="Выберите роль"
-          onChange={formik.handleChange}
-          value={formik.values.role}
-        >
-          {Object.values(Role).map((role) => (
-            <SelectItem key={role}>{role}</SelectItem>
-          ))}
-        </Select>
+          <span className="text-md px-1 font-medium text-danger">
+            {errors.name || errors.phone || errors.email || errors.password || errors.role}
+          </span>
 
-        <Button type="submit" color="primary">
-          Войти
-        </Button>
-      </form>
-    </div>
+          <Button className="font-medium" variant="shadow" color="primary" size="lg" type="submit">
+            Зарегистрировать
+          </Button>
+        </form>
+      )}
+    </Formik>
   )
 }
