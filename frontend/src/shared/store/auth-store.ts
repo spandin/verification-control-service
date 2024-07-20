1afb2dcb61beb8a5d496f640be666b9bb718befa
Stanislau <willstesi@gmail.com>
2024-07-20 15:58:57 +0300

    feat: create auth store

---
+++ frontend/src/shared/store/auth-store.ts
@@ -0,0 +1,31 @@
+import { create } from "zustand"
+import { devtools, persist } from "zustand/middleware"
+
+interface StoreState {
+  token: string | null
+  isAuth: boolean
+  isLoading: boolean
+
+  setToken: (token: string | null) => void
+  setIsAuth: (isAuth: boolean) => void
+  setLoading: (isLoading: boolean) => void
+}
+
+export const useAuthStore = create<StoreState>()(
+  devtools(
+    persist(
+      (set) => ({
+        token: null,
+        isAuth: false,
+        isLoading: false,
+
+        setToken: (token) => set({ token }),
+        setIsAuth: (isAuth) => set({ isAuth }),
+        setLoading: (isLoading) => set({ isLoading }),
+      }),
+      {
+        name: "auth-store",
+      },
+    ),
+  ),
+)
