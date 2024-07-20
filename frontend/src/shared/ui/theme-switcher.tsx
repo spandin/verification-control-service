6a05b820fbf937a54d217d2999bc1e7e7996a1e8
Stanislau <willstesi@gmail.com>
2024-07-21 00:25:06 +0300

    feat: add switch

--- frontend/src/shared/ui/theme-switcher.tsx
+++ frontend/src/shared/ui/theme-switcher.tsx
@@ -1,3 +1,5 @@
+import { Switch } from "@nextui-org/react"
+import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 import { useEffect, useState } from "react"
 
@@ -12,10 +14,14 @@ export function ThemeSwitcher() {
   if (!mounted) return null
 
   return (
-    <div>
-      The current theme is: {theme}
-      <button onClick={() => setTheme("light")}>Light Mode</button>
-      <button onClick={() => setTheme("dark")}>Dark Mode</button>
-    </div>
+    <Switch
+      className="absolute right-4 top-6"
+      isSelected={theme === "dark" ? true : false}
+      onClick={() => (theme === "light" ? setTheme("dark") : setTheme("light"))}
+      size="md"
+      color="primary"
+      startContent={<Sun />}
+      endContent={<Moon />}
+    />
   )
 }
