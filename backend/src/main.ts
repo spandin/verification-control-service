cd4931ef3c83c7807d360058a8beb89ca5a35572
Stanislau <willstesi@gmail.com>
2024-07-20 15:56:28 +0300

    feat: add cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const corsOptions: CorsOptions = {
+    origin: ['http://localhost:5173'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  }
+
+  app.enableCors(corsOptions)
 
   const config = new DocumentBuilder()
     .setTitle('VSC')
