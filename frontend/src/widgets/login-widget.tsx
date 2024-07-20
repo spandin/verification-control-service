1898c081826b15ca6c9406ab8c550da38f18492c
Stanislau <willstesi@gmail.com>
2024-07-21 00:25:30 +0300

    feat: add widget

--- frontend/src/widgets/login-widget.tsx
+++ frontend/src/widgets/login-widget.tsx
@@ -1,8 +1,10 @@
 import { LoginForm } from "@/feauters/auth/login"
+import { ThemeSwitcher } from "@/shared/ui/theme-switcher"
 
 const LoginWidget = () => {
   return (
     <div className="flex h-screen flex-col items-center justify-center px-6">
+      <ThemeSwitcher />
       <LoginForm />
     </div>
   )
