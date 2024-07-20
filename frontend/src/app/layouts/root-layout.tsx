ef72c6d15425fc69998ee504b98de67ba6d01acb
Stanislau <willstesi@gmail.com>
2024-07-20 23:46:53 +0300

    feat: work with layout

--- frontend/src/app/layouts/root-layout.tsx
+++ frontend/src/app/layouts/root-layout.tsx
@@ -1,29 +1,24 @@
-import { Link } from "@nextui-org/link";
+import { LoginForm } from "@/feauters/auth/login"
+import { useAuth } from "@/shared/hooks/use-auth"
+import { Navbar } from "@/widgets/navbar"
 
-import { Navbar } from "@/widgets/navbar";
+export const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  const { isAuth } = useAuth()
 
-export default function DefaultLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+  if (!isAuth) {
+    return (
+      <div className="flex h-screen flex-col items-center justify-center px-6">
+        <LoginForm />
+      </div>
+    )
+  }
+
   return (
-    <div className="relative flex flex-col h-screen">
+    <div className="bg-backgroud flex h-screen flex-col-reverse text-foreground md:flex-row">
       <Navbar />
-      <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
+      <main className="container max-w-4xl flex-grow border-r-slate-200 p-6 md:border-r-2">
         {children}
       </main>
-      <footer className="w-full flex items-center justify-center py-3">
-        <Link
-          isExternal
-          className="flex items-center gap-1 text-current"
-          href="https://nextui-docs-v2.vercel.app?utm_source=next-pages-template"
-          title="nextui.org homepage"
-        >
-          <span className="text-default-600">Powered by</span>
-          <p className="text-primary">NextUI</p>
-        </Link>
-      </footer>
     </div>
-  );
+  )
 }
