816c67699a11f065c902b84d0cc533e364c002c0
Stanislau <willstesi@gmail.com>
2024-07-20 23:47:06 +0300

    feat: add toast container

--- frontend/src/app/provider.tsx
+++ frontend/src/app/provider.tsx
@@ -1,8 +1,15 @@
 import { NextUIProvider } from "@nextui-org/system"
 import { useNavigate } from "react-router-dom"
+import { RootLayout } from "./layouts/root-layout"
+import { ToastContainer } from "react-toastify"
 
 export function Provider({ children }: { children: React.ReactNode }) {
   const navigate = useNavigate()
 
-  return <NextUIProvider navigate={navigate}>{children}</NextUIProvider>
+  return (
+    <NextUIProvider navigate={navigate}>
+      <RootLayout>{children}</RootLayout>
+      <ToastContainer />
+    </NextUIProvider>
+  )
 }
