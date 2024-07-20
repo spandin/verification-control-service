a1ce7ee5adb08a0d2ac8a7f1a9120d51ed0851e8
Stanislau <willstesi@gmail.com>
2024-07-20 23:50:21 +0300

    feat: refactoring

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,5 @@
-import { LoginForm } from "@/feauters/auth/login"
 import { RegisterForm } from "@/feauters/auth/register"
 
 export default function IndexPage() {
-  return (
-    <div>
-      <LoginForm /> <RegisterForm />
-    </div>
-  )
+  return <RegisterForm />
 }
