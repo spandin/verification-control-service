97bbb6656984e9866670f7dfce424ac28d6ab08c
Stanislau <willstesi@gmail.com>
2024-07-20 17:28:11 +0300

    feat: change get token method

--- frontend/src/shared/api/axios-instance.ts
+++ frontend/src/shared/api/axios-instance.ts
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { SITE_CONFIG } from "../config/site"
+import { useAuthStore } from "../store/auth-store"
 
 const axiosInstance = axios.create({
   baseURL: SITE_CONFIG.BASE_URL,
@@ -7,7 +8,7 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token")
+    const token = useAuthStore.getState().token
 
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
