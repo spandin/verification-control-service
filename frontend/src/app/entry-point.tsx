1782e5c6f5baa0ff686c7474508d183ebfad2a68
Stanislau <willstesi@gmail.com>
2024-07-20 15:57:16 +0300

    feat: add toast container

--- frontend/src/app/entry-point.tsx
+++ frontend/src/app/entry-point.tsx
@@ -1,10 +1,12 @@
 import "@/shared/styles/globals.css"
+import "react-toastify/dist/ReactToastify.css"
 
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Provider } from "./provider.tsx"
 import IndexPage from "@/pages/index.tsx"
+import { ToastContainer } from "react-toastify"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -13,6 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <Routes>
           <Route element={<IndexPage />} path="/" />
         </Routes>
+        <ToastContainer />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
