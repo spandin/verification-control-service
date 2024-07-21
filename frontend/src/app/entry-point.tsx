ac727b53d21c70fcae6da57582487a481f448b65
Stanislau <willstesi@gmail.com>
2024-07-21 11:59:10 +0300

    feat: add register sw

--- frontend/src/app/entry-point.tsx
+++ frontend/src/app/entry-point.tsx
@@ -5,6 +5,15 @@ import ReactDOM from "react-dom/client"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Provider } from "./provider.tsx"
 import IndexPage from "@/pages/index.tsx"
+import { registerSW } from "virtual:pwa-register"
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("У вас старая версия приложения. Обновить?")) {
+      updateSW(true)
+    }
+  },
+})
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
