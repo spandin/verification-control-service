8bcd2cdb0037835e745d7b944405846df6105fc7
Stanislau Pandzin <86323835+spandin@users.noreply.github.com>
2024-07-22 11:34:15 +0300

    feat: add localhost to host

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -1,5 +1,5 @@
 export default () => ({
   JWT_SECRET: process.env.JWT_SECRET,
   DATABASE_URL: process.env.DATABASE_URL,
-  HOST: process.env.HOST,
+  HOST: process.env.HOST || 'http://localhost:3000',
 })
