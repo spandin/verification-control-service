d9b6d23733cd33f68332b3c1a20e74f1cdf96378
Stanislau Pandzin <86323835+spandin@users.noreply.github.com>
2024-07-22 09:32:55 +0300

    feat: decomposition

--- frontend/src/feauters/auth/login/ui/login-form.tsx
+++ frontend/src/feauters/auth/login/ui/login-form.tsx
@@ -1,22 +1,21 @@
-import { Button } from "@nextui-org/button"
-import { Input } from "@nextui-org/input"
 import { Formik } from "formik"
 import * as Yup from "yup"
 import { useAuth } from "@/shared/hooks/use-auth"
 import PasswordInput from "@/shared/ui/password-input"
 import { SITE_CONFIG } from "@/shared/config/site"
-import { PopoverTrigger, PopoverContent, Popover } from "@nextui-org/react"
+import HelpButton from "./compose/help-button"
+import { Button, Input } from "@nextui-org/react"
 
 const validationSchema = Yup.object({
   email: Yup.string()
-    .email("Введите корректный email адрес.")
-    .required("Email обязятелен для заполнения.")
-    .min(5, "Минимальная длинна email - 5 символов.")
-    .max(32, "Максимальная длинна пароля - 32 символа."),
+    .email("Введите корректный email адрес")
+    .required("Email обязятелен для заполнения")
+    .min(5, "Минимальная длинна email - 5 символов")
+    .max(32, "Максимальная длинна пароля - 32 символа"),
   password: Yup.string()
-    .required("Пароль обязятелен для заполнения.")
-    .min(6, "Минимальная длинна пароля - 6 символов.")
-    .max(32, "Максимальная длинна пароля - 32 символа."),
+    .required("Пароль обязятелен для заполнения")
+    .min(6, "Минимальная длинна пароля - 6 символов")
+    .max(32, "Максимальная длинна пароля - 32 символа"),
 })
 
 export const LoginForm = () => {
@@ -38,8 +37,8 @@ export const LoginForm = () => {
       {({ errors, values, handleChange, handleBlur, handleSubmit }) => (
         <form className="flex w-full max-w-sm flex-col gap-2" onSubmit={handleSubmit} noValidate>
           <div className="mb-4 flex flex-col items-start justify-start px-1">
-            <div className="flex flex-row items-center justify-start gap-3">
-              <div className="h-4 w-4 rotate-45 bg-warning" />
+            <div className="flex flex-row items-center justify-start gap-2">
+              <img className="max-w-6" src="/favicon.svg" />
               <h1 className="text-2xl font-semibold text-slate-800 dark:text-slate-200">
                 {SITE_CONFIG.description}
               </h1>
@@ -72,7 +71,7 @@ export const LoginForm = () => {
             />
           </div>
 
-          <span className="text-md px-1 font-medium text-danger">
+          <span className="px-1 text-base font-normal text-danger">
             {errors.email || errors.password}
           </span>
 
@@ -80,34 +79,7 @@ export const LoginForm = () => {
             Войти
           </Button>
 
-          <Popover
-            showArrow
-            backdrop="blur"
-            placement="bottom"
-            classNames={{
-              base: ["before:bg-default-200"],
-              content: ["py-3 px-4 max-w-sm"],
-            }}
-          >
-            <PopoverTrigger>
-              <Button className="font-semibold" variant="light" size="lg">
-                Помощь
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent>
-              {(titleProps) => (
-                <div className="px-1 py-2">
-                  <h3 className="text-base font-semibold" {...titleProps}>
-                    Как войти?
-                  </h3>
-                  <div className="text-md">
-                    Обратитесь к управляющим, возможно ваши данные для входа уже высланы на ваш
-                    Email
-                  </div>
-                </div>
-              )}
-            </PopoverContent>
-          </Popover>
+          <HelpButton />
         </form>
       )}
     </Formik>
