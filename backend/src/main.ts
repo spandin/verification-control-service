f9c3bc5b69493e72befe55a5abbefb60ea267354
Stanislau Pandzin <86323835+spandin@users.noreply.github.com>
2024-07-22 16:04:33 +0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   const corsOptions: CorsOptions = {
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://verification-control-service.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
