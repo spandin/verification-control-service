3df2d3f3793050d2413aa3318156bd5ca14f1380
Stanislau Pandzin <86323835+spandin@users.noreply.github.com>
2024-07-24 15:23:00 +0300

    feat: registration transfer to the user module

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,10 +1,71 @@
-import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common'
+import * as bcrypt from 'bcrypt'
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
-import { UserDto } from './dto/index.dto'
+import { UserDto } from './dto/user.dto'
+import { CreateUserDto } from './dto/create-user.dto'
+import { AuthService } from 'src/auth/auth.service'
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly authService: AuthService,
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<{ token: string }> {
+    const { phone, email, password, name, role: roleName } = createUserDto
+
+    const role = await this.prisma.role.findUnique({
+      where: { name: roleName },
+    })
+    if (!role) {
+      throw new BadRequestException('Invalid role. Must be a valid role.')
+    }
+
+    if (role.name === 'администратор') {
+      throw new BadRequestException(
+        'Invalid request. Cannot register as an administrator or invalid role.',
+      )
+    }
+
+    const allRoles = await this.prisma.role.findMany({
+      select: {
+        name: true,
+      },
+    })
+
+    const validRoles = allRoles.map((role) => role.name)
+    if (!validRoles.includes(role.name)) {
+      throw new BadRequestException('Invalid request. Invalid role.')
+    }
+
+    const existingUser = await this.prisma.user.findUnique({ where: { email } })
+    if (existingUser) {
+      throw new ConflictException('User already exists.')
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const user = await this.prisma.user.create({
+      data: {
+        phone,
+        email,
+        password: hashedPassword,
+        name,
+        role: {
+          connect: { id: role.id },
+        },
+      },
+    })
+
+    return this.authService.generateAndSaveToken(user)
+  }
 
   async getUsers(): Promise<UserDto[]> {
     const users = await this.prisma.user.findMany({
