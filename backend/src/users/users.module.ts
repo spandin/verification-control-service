c0c6456565da3fc09acbab489ce2955f3c0343c9
Stanislau Pandzin <86323835+spandin@users.noreply.github.com>
2024-07-24 15:31:21 +0300

    feat: add imports and services

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common'
 import { UsersService } from './users.service'
 import { UsersController } from './users.controller'
 import { PrismaModule } from 'src/prisma/prisma.module'
+import { AuthService } from 'src/auth/auth.service'
+import { JwtModule } from '@nestjs/jwt'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+      }),
+    }),
+  ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, AuthService],
 })
 export class UsersModule {}
