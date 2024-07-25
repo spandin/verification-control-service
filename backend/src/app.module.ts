b690a870209e23a05d4902c385506eecb05b0b36
Stanislau <willstesi@gmail.com>
2024-07-25 17:54:46 +0300

    feat: change path imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,11 @@ import { ScheduleModule } from '@nestjs/schedule'
 import { ConfigModule } from '@nestjs/config'
 import { PrismaModule } from './prisma/prisma.module'
 import { AuthModule } from './auth/auth.module'
-import { WakeUpModule } from './wake-up/wake-up.module'
-import { SpecModule } from './spec/spec.module'
 import { DevicesModule } from './devices/devices.module'
 import { RoleModule } from './role/role.module'
 import { UsersModule } from './users/users.module'
+import { WakeUpModule } from './helpers/wake-up/wake-up.module'
+import { SpecModule } from './helpers/spec/spec.module'
 
 @Module({
   imports: [
