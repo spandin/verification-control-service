7cd18aecf1af0e883439688c8e52424447c03615
Stanislau Pandzin <86323835+spandin@users.noreply.github.com>
2024-11-05 10:51:50 +0300

    feat: add routes

--- frontend/src/app/entry-point.tsx
+++ frontend/src/app/entry-point.tsx
@@ -6,6 +6,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Provider } from "./provider.tsx"
 import IndexPage from "@/pages/index.tsx"
 import { registerSW } from "virtual:pwa-register"
+import LoginPage from "@/pages/login.tsx"
+import RegisterPage from "@/pages/register.tsx"
 
 const updateSW = registerSW({
   onNeedRefresh() {
@@ -21,6 +23,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <Provider>
         <Routes>
           <Route element={<IndexPage />} path="/" />
+          <Route element={<LoginPage />} path="/login" />
+          <Route element={<RegisterPage />} path="/register" />
         </Routes>
       </Provider>
     </BrowserRouter>
